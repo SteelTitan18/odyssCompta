5f05d1d0a1ec63ff20006a4823678b18945be86f
steeltitanunbrk <fortune18000@gmail.com>
2023-05-11 13:37:44 +0000

    Mise en place de la connexion avec firebase et afichage de la valeur en caisse

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:odysscompta/Coffer.dart';
 import 'package:odysscompta/LostConnectionPage.dart';
 import 'package:odysscompta/Selling.dart';
 import 'package:odysscompta/Simple_Buying.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'RecetteProvider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(
@@ -30,11 +31,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Odyssée des Saveurs : Comptabilité',
         theme: ThemeData(
-          primarySwatch: Colors.brown,
+          primarySwatch: Colors.amber,
         ),
         home: ChangeNotifierProvider<RecetteProvider>(
           create: (_) => RecetteProvider(),
@@ -49,145 +52,181 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
+    CollectionReference accounts =
+        FirebaseFirestore.instance.collection('accounts');
+
     return Scaffold(
         extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: Text(title),
-          automaticallyImplyLeading: false
-        ),
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            crossAxisSpacing: 16.0,
-            mainAxisSpacing: 16.0,
-            children: [
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.brown,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      minimumSize: const Size(100, 0),
-                    ),
-                    onPressed: () async {
-                      try {
-                        await InternetAddress.lookup('www.google.com');
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SellPage()),
-                        );
-                      } on SocketException {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LostConnectionPage()),
-                        );
-                      }
-                    },
-                    child: const Text(
-                      "Vente",
-                      style: TextStyle(
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.brown,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      minimumSize: const Size(100, 0),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Simple_BuyPage()),
-                      );
-                    },
-                    child: const Text(
-                      "Dépense",
-                      style: TextStyle(
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.brown,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      minimumSize: const Size(100, 0),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      "Statistiques",
-                      style: TextStyle(
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.brown,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      minimumSize: const Size(100, 0),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const CofferPage()),
-                      );
-                    },
-                    child: const Text(
-                      "Caisse",
-                      style: TextStyle(
-                        color: Color(0xffffffff),
-                      ),
-                    ),
-                  )),
-            ],
-          ),
-          Container(
-              padding: const EdgeInsets.only(top: 25, left: 8, right: 8),
-              width: 500,
-              height: 100,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.brown,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  minimumSize: const Size(100, 0),
-                ),
-                onPressed: () {
-                  account_doc();
-                },
-                child: const Text(
-                  "Fichier de compte",
-                  style: TextStyle(
-                    color: Color(0xffffffff),
-                  ),
-                ),
-              ))
-        ])));
+        appBar: AppBar(title: Text(title), automaticallyImplyLeading: false),
+        body: SingleChildScrollView(
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+              FutureBuilder(
+                  future: accounts.doc('accounts').get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      Map<String, dynamic> cash =
+                          snapshot.data!.data() as Map<String, dynamic>;
+
+                      return MediaQuery(
+                          data: MediaQuery.of(context).copyWith(),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.blueAccent,
+                            ),
+                            margin: const EdgeInsets.only(top: 150, bottom: 16),
+                            //padding: const EdgeInsets.all(8.0),
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            height: 150,
+                            child: Center(
+                                child: Text(
+                              "Caisse\n${cash['fund']}\tFCFA",
+                              style: const TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            )),
+                          ));
+                    }
+
+                    return MediaQuery(
+                        data: MediaQuery.of(context).copyWith(),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blueAccent,
+                          ),
+                          margin: const EdgeInsets.only(top: 150, bottom: 16),
+                          //padding: const EdgeInsets.all(8.0),
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: 150,
+                          child:
+                              const Center(child: CircularProgressIndicator()),
+                        ));
+                  }),
+              MediaQuery(
+                  data: MediaQuery.of(context).copyWith(),
+                  child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: 100,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.cyan,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          minimumSize: const Size(100, 0),
+                        ),
+                        onPressed: () async {
+                          try {
+                            await InternetAddress.lookup('www.google.com');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SellPage()),
+                            );
+                          } on SocketException {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LostConnectionPage()),
+                            );
+                          }
+                        },
+                        child: const Text(
+                          "Vente",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ))),
+              MediaQuery(
+                  data: MediaQuery.of(context).copyWith(),
+                  child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: 100,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.cyan,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          minimumSize: const Size(100, 0),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Simple_BuyPage()),
+                          );
+                        },
+                        child: const Text(
+                          "Dépense",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ))),
+              MediaQuery(
+                  data: MediaQuery.of(context).copyWith(),
+                  child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: 100,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.cyan,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          minimumSize: const Size(100, 0),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "Statistiques",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      ))),
+              MediaQuery(
+                  data: MediaQuery.of(context).copyWith(),
+                  child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: 100,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.cyan,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          minimumSize: const Size(100, 0),
+                        ),
+                        onPressed: () {
+                          account_doc();
+                        },
+                        child: const Text(
+                          "Fichier de compte",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xffffffff),
+                          ),
+                        ),
+                      )))
+            ]))));
   }
 }
